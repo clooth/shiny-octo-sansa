992d6d48d40a80635be315ed4a9872d1984baef5
Nico Hämäläinen <zenverse@gmail.com>
2013-10-09 00:29:18 +0300

    Initial Commit

---
+++ Cometfight/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Cometfight
+//
+//  Created by Nico Hämäläinen on 10/9/13.
+//  Copyright (c) 2013 Nico Hämäläinen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CometfightTests/CometfightTests.m
@@ -0,0 +1,34 @@
+//
+//  CometfightTests.m
+//  CometfightTests
+//
+//  Created by Nico Hämäläinen on 10/9/13.
+//  Copyright (c) 2013 Nico Hämäläinen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CometfightTests : XCTestCase
+
+@end
+
+@implementation CometfightTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
